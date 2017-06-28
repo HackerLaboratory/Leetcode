d3bc7b42ed4a8c0aa1296dd3de6e43e7d98d2a33
xumenger <xumenger@126.com>
2017-06-28 23:54:59 +0800

    关于自动化测试的思考

--- 20170628-median-of-two-sorted-arrays/main.c
+++ 20170628-median-of-two-sorted-arrays/main.c
@@ -5,7 +5,7 @@ double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Si
     return 0.0;
 }
 
-int Compare(double expect, double real, char* s){
+int compare(double expect, double real, char* s){
     if(expect - real < 0.0001){
         printf("%s，比较通过！预期值 = 实际值：%f\n", s, expect);
         return 0;
@@ -16,11 +16,19 @@ int Compare(double expect, double real, char* s){
     }
 }
 
+//如果进一步优化，可以将测试用例（两个数组、一个预期输出）按一定格式维护在文件中
+//程序只是加载数据，然后比较，这样就不需要修改程而方便的测试各种数据了
+//这样就更加符合自动化测试的思维了
 int main(){
     int testNums1_1[2] = {1, 3};
     int testNums2_1[1] = {2};
-    double real = findMedianSortedArrays(testNums1_1, 2, testNums2_1, 1);
-    Compare(2, real, "测试用例1");
+    double real_1 = findMedianSortedArrays(testNums1_1, 2, testNums2_1, 1);
+    compare(2, real_1, "测试用例1");
+
+    int testNums1_2[9] = {0, 2, 4, 6, 8, 10, 20, 100, 1000};
+    int testNums2_2[5] = {1, 3, 5, 7, 9};
+    double real_2 = findMedianSortedArrays(testNums1_2, 9, testNums2_2, 5);
+    compare(6.5, real_2, "测试用例2");
 
     return 0;
 }
